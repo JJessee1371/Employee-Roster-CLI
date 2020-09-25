815b29d96925ddcc3b14e76d0308bb1291ca4d69
Jon Jessee <tjessee7624@gmail.com>
2020-09-25 15:09:43 -0600

    Extended employee class to Engineer and ensure subclass passes all tests.

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,21 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// Engineer class contructor importing employee class
+const Employee = require('./Employee');
+
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+    super(name, id, email);
+    this.github = github;
+    };
+
+    getRole() {
+        return 'Engineer';
+    };
+
+    getGithub() {
+        return this.github;
+    };
+};
+
+const engineer = new Engineer('Jon', 456, 'test@test.com', 'www.stuff.com');
+module.exports = Engineer;
+
