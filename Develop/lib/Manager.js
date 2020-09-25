2fb0b9414b86a9280a2db4b579c016541ec28958
Jon Jessee <tjessee7624@gmail.com>
2020-09-25 14:58:07 -0600

    Extended Employee class to Manager and wrote subclass to pass all tests.

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,24 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+// Manager class constructor requires Employee export
+const Employee = require('./Employee');
+class Manager extends Employee {
+    constructor(name, id, email, officeNumber) {
+        super(name, id, email);
+
+        this.officeNumber = officeNumber;
+    };
+    
+    getRole() {
+        return 'Manager';
+    };
+
+    getOfficeNumber() {
+        return this.officeNumber;
+    };
+};
+
+const e = new Manager('Jon', 123, 'test@test.com', 987654321);
+
+module.exports = Manager;
+
+//Office number property
+//getRole will return manager
