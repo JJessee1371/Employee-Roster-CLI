1dea95670f696feac4005b686d157a58da259f4a
Jon Jessee <tjessee7624@gmail.com>
2020-09-26 21:35:02 -0600

    Set up inquirer prompts to gather information about the team members.

--- Develop/app.js
+++ Develop/app.js
@@ -10,9 +10,150 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+//Recursion will be key to this, wrap the prompt in an if statement that will provide an exit functionality
+//whenever the user answers that they would not like to continue.
 
-// Write code to use inquirer to gather information about the development team members,
+//Instead of having the entire thing wrapped in a single recursive function, split it into different types of employees
+// this way the manager is onlly asked for once and then based on the users choice they can add in another engineer 
+//or an intern. 
+//Chain events with a .then statement so that the mamager function is always executed before anyting else?
+
+
+// Write code to use inquirer to gather information about the development team membeers,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+//Prompt for manager information(only executed once)
+function getManager() {
+    inquirer.prompt([
+        {
+            message: "What is the managers name?",
+            type: 'input',
+            name: 'managername'
+        },
+        {
+            message: "What is their ID number?",
+            type: 'input',
+            name: 'managerid'
+        },
+        {
+            message: "What is thier email address?",
+            type: 'input',
+            name: 'manageremail'
+        },
+        {
+            message: 'What is their office phone number?',
+            type: 'input',
+            name: 'officenumber'
+        }
+    ]);
+};
+
+//Prompt for emplyee information with choices for either an intern or engineer
+//Recursive function that exits only when the user declines to enter further info
+function getEmployee() {
+    const choice = inquirer.prompt([
+        {
+            type: 'list',
+            choices: [
+                'Intern',
+                'Engineer'
+            ],
+            name: 'type'
+        },
+    ]);
+
+    if(choice.type == 'Intern') {
+        const intern = inquirer.prompt([
+            {
+                message: "What is the interns' name?",
+                type: 'input',
+                name: 'internname'
+            },
+            {
+                message: 'What is their ID number?',
+                type: 'input',
+                name: 'internid'
+            },
+            {
+                message: 'What is their email address?',
+                type: 'input',
+                name: 'internemail'
+            },
+            {
+                message: 'What school do they attend?',
+                type: 'input',
+                name: 'school'
+            },
+            {
+                message: 'Would you like to enter another employee?',
+                type: 'confirm',
+                name: 'confirm',
+                default: 'false'
+            }
+        ]);
+    } else {
+        const engineer = inquirer.prompt([
+            {
+                message: "What is the engineers' name?",
+                type: 'input',
+                name: 'engineername'
+            },
+            {
+                message: 'What is their ID number?',
+                type: 'input',
+                name: 'engineerid'
+            },
+            {
+                message: 'What is their email address?',
+                type: 'input',
+                name: 'engineeremail'
+            },
+            {
+                message: 'What is their personal GitHub URL?',
+                type: 'input',
+                name: 'github'
+            },
+            {
+                message: 'Would you like to enter another employee?',
+                type: 'confirm',
+                name: 'confirm',
+                default: 'false'
+            }
+        ]);
+    };
+
+    if(intern.confirm == true) {
+        getEmployee();
+    } else {
+        //call render function here or exit process
+    };
+
+    if(engineer.confirm == true) {
+        getEmployee();
+    } else {
+        //execute some other function or exit process
+    };
+};
+
+// function promptUser() {
+//     if () {
+//     inquirer.prompt{[
+//         {
+//             message: 'What is the managers name',
+//             type: 'input',
+//             name: 'managername'       
+//     },
+
+//     {
+//         message: 'What is the managers ID number?',
+//         type: 'input',
+//         name: 'managerID'
+//     }
+//     ]}
+
+//     promptUser();
+//     }
+// }
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
